a7a72916bff6b2ac89513927b3fb403a39c9202e
Michał Muzykant <michal.muzykant@edu.uekat.pl>
2024-02-25 15:39:37 +0100

    zadania 1-6

---
+++ Nowy folder/zad_2.py
@@ -0,0 +1,15 @@
+
+# Stworzyć funkcję, która przyjmie 2 argumenty typu int , a następnie zwróci
+# wynik mnożenia obu liczb.
+
+
+
+def suma (a,b):
+    return a+b
+
+
+sumowanie = suma(3, 4)
+
+print(sumowanie)
+
+
